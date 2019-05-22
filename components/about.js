4251491c041ac36526aef9655eaba90b28e7214e
Reed James <ReedTJames1234@gmail.com>
2019-05-22 17:11:27 -0500

    WIP: bug on about dom, working on link to react now

--- components/about.js
+++ components/about.js
@@ -1,44 +1,59 @@
 //aboutpage.js
 // ################################################################
-class FeatureLink {
+class AboutFeatureLink {
   constructor(link) {
     this.link = link;
-    console.log(this.link);
+    // console.log(this.link);
 
     this.data = this.link.dataset.tab;
-    console.log(this.data);
-    this.featureElement = document.querySelector(
-      `.feature-content[data-tab='${this.data}']`
+    // console.log(this.data);
+    // this.featureElement = document.querySelector(
+    //   `.feature-content[data-tab='${this.data}']`
+    // );
+    if (this.data === "all") {
+      this.blurbs = document.querySelectorAll(".about-bottom-blurb");
+    } else {
+      this.blurbs = document.querySelectorAll(
+        `.about-bottom-blurb[data-tab=${this.data}]`
+      );
+    }
+
+    this.blurbs = Array.from(this.blurbs).map(
+      blurb => new AboutFeatureItem(blurb)
     );
-    this.featureItem = new FeatureItem(this.featureElement);
-    this.link.addEventListener("click", () => this.select());
+    this.link.addEventListener("click", () => this.selectFeature());
   }
 
-  select() {
-    const links = document.querySelectorAll(".feature");
-    links.forEach(link => link.classList.remove("active"));
-    this.link.classList.add("active");
-    this.featureItem.select();
+  selectFeature() {
+    const links = document.querySelectorAll(".feature-about");
+    links.forEach(link => link.classList.remove("active-about"));
+    const aboutBlurbs = document.querySelectorAll(".about-bottom-blurb");
+    console.log(aboutBlurbs);
+    aboutBlurbs.forEach(blurb => (blurb.style.display = "none"));
+    this.link.classList.add("active-about-blurb");
+    this.aboutBlurbs.forEach(blurb => blurb.selectBlurb());
+    // this.link.classList.add("active");
+    // this.featureItem.select();
   }
 }
 // ################################################################
 
-class FeatureItem {
+class AboutFeatureItem {
   constructor(element) {
     this.element = element;
   }
 
-  select() {
-    const items = document.querySelectorAll(".feature-content");
-    items.forEach(link => link.classList.remove("active-item"));
-    this.element.classList.add("active-item");
+  selectBlurb() {
+    this.element.style.display = "flex";
+    // const items = document.querySelectorAll(".feature-content");
+    // items.forEach(link => link.classList.remove("active-item"));
   }
 }
 
 // ################################################################
-const links = document.querySelectorAll(".feature");
-console.log(links);
+const links = document.querySelectorAll(".feature-about");
+// console.log(links);
 
 links.forEach(link => {
-  new FeatureLink(link);
+  new AboutFeatureLink(link);
 });
